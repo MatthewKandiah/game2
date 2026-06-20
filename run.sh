96d56e6a8a4ec2b635d4eec53d0d1cf160e1d466
Matthew Kandiah <mj.kandiah@googlemail.com>
2026-06-20 20:37:06 +0100

    sometimes I want to just build

--- run.sh
+++ run.sh
@@ -2,17 +2,7 @@
 
 set -eu
 
-rm -rf ./build
-mkdir ./build
-
-echo "Compiling shaders..."
-glslc -o build/vert.spv shaders/main.vert -g
-glslc -o build/sprite-frag.spv shaders/sprite.frag -g
-glslc -o build/mask-frag.spv shaders/mask.frag -g
-
-echo "Building main..."
-# -o:speed / -o:minimal
-odin build main -out:build/main.bin -debug -o:minimal
-
+./build.sh
 echo "Running main..."
 ./build/main.bin
+
